34b4acf542979845768338f3e58c814560e24ca7
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-10-17 17:12:01 +0900

    fix: コードミスを修正

--- cogs/ban_member.py
+++ cogs/ban_member.py
@@ -9,11 +9,14 @@ class ban_member(commands.Cog):
     
     @app_commands.command(name="ban_member", description="Banされたユーザー一覧を表示します。")
     async def ban_members(self, i: discord.Interaction):
+        m = []
         try:
             if i.guild.channels.permissions_for(i.user) == discord.Permissions.ban_members:
                 if i.guild.channels.permissions_for(i.guild.get_member(self.bot.user.id)) == discord.Permissions.ban_members:
                     async for entry in guild.bans(limit=150):
-                        print(entry.user, entry.reason)
+                        m.append(f"{entry.user.name}, ")
+                    send_content = "".join(m)
+                    await i.response.send_message(send_content)
                 else:
                     await i.response.send_message("データを取得できませんでした(Botの権限がないなどの原因で)", ephemeral=True)
             else:
