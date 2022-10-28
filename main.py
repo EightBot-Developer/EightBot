01dc47ecb469b8da88f24cd00db2187879ed008e
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-10-28 18:37:47 +0900

    delete: message_content

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class EightBot(commands.Bot):
 
 bot = EightBot(
     command_prefix="eg!",
-    intents=Intents(auto_moderation=False, typing=False, message_content=True),
+    intents=Intents(auto_moderation=False, typing=False),
     activity=Activity(
         type=ActivityType.watching,
         name="起動準備をしています...",
