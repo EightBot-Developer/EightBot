034087300929794dc9190d2778f615ff4d68c442
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-10-30 08:07:13 +0900

    bug fix

--- cogs/ban_member.py
+++ cogs/ban_member.py
@@ -10,19 +10,18 @@ class ban_member(commands.Cog):
     @app_commands.command(name="ban_member", description="Banされたユーザー一覧を表示します。")
     async def ban_members(self, i: discord.Interaction):
         m = []
-        try:
-            if i.guild.channels.permissions_for(i.user) == discord.Permissions.ban_members:
-                if i.guild.channels.permissions_for(i.guild.get_member(self.bot.user.id)) == discord.Permissions.ban_members:
-                    async for entry in i.guild.bans(limit=150):
-                        m.append(f"{entry.user.name}, ")
-                    send_content = "".join(m)
-                    await i.response.send_message(send_content)
-                else:
-                    await i.response.send_message("データを取得できませんでした(Botの権限がないなどの原因で)", ephemeral=True)
+        if i.channel.permissions_for(i.user) == discord.Permissions.ban_members:
+            if i.channel.permissions_for(i.guild.get_member(self.bot.user.id)) == discord.Permissions.ban_members:
+                async for entry in i.guild.bans(limit=150):
+                    m.append(f"{entry.user.name}, ")
+                send_content = "".join(m)
+                if len(m) == 0:
+                    send_content = "Banされたユーザーはいません。"
+                await i.response.send_message(send_content)
             else:
-                await i.response.send_message("データを取得できませんでした(権限がないなどの原因で)", ephemeral=True)
-        except:
-            await i.response.send_message("データを取得できませんでした(権限がないなどの原因で)", ephemeral=True)
+                await i.response.send_message("データを取得できませんでした(Botの権限がないなどの原因で)", ephemeral=True)
+        else:
+            await i.response.send_message("データを取得できませんでした。(権限がないなどの原因)", ephemeral=True)
 
 
 async def setup(bot: commands.Bot) -> None:
