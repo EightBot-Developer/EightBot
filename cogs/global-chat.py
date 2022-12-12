af70bf5076d248f0e7f754280facd40423dd6f6b
Dec-dog <eightbot1644@gmail.com>
2022-12-12 19:04:58 +0900

    🐛fix: いろいろ

--- cogs/global-chat.py
+++ cogs/global-chat.py
@@ -28,45 +28,45 @@ class global_chat_cog(commands.Cog):
             return
         if message.channel.type == discord.ChannelType.text:
             for channel in self.bot.get_all_channels():
-                if global_chat(channel.id) == "True":
-                    if channel.id == message.channel.id:
-                        continue
-                    embed = discord.Embed(
-                        description=message.content, color=0x3498DB)
-                    if hasattr(message.author.avatar, 'key'):
-                        embed.set_author(name="{}#{}".format(message.author.name, message.author.discriminator), icon_url="https://media.discordapp.net/avatars/{}/{}.png?size=1024".format(
-                            message.author.id, message.author.avatar.key))
-                    else:
-                        embed.set_author(name="{}#{}".format(
-                            message.author.name, message.author.discriminator), url=f"https://discord.com/users/{message.author.id}")
-                    if hasattr(message.guild.icon, 'key'):
-                        embed.set_footer(text="{} / mID:{}".format(message.guild.name, message.id),
+                  if global_chat(channel.id) == "True":
+                        if channel.id == message.channel.id:
+                            return
+                        else:
+                            embed = discord.Embed(description=message.content, color=0x3498DB)
+                            if hasattr(message.author.avatar, 'key'):
+                                embed.set_author(name="{}#{}".format(message.author.name, message.author.discriminator), icon_url="https://media.discordapp.net/avatars/{}/{}.png?size=1024".format(
+                                    message.author.id, message.author.avatar.key))
+                            else:
+                                embed.set_author(name="{}#{}".format(
+                                    message.author.name, message.author.discriminator), url=f"https://discord.com/users/{message.author.id}")
+                            if hasattr(message.guild.icon, 'key'):
+                                embed.set_footer(text="{} / mID:{}".format(message.guild.name, message.id),
                                          icon_url="https://media.discordapp.net/icons/{}/{}.png?size=1024".format(message.guild.id, message.guild.icon.key))
-                    else:
-                        embed.set_footer(
-                            text="{} / mID:{}".format(message.guild.name, message.id))
-                    if message.attachments != []:
-                        embed.set_image(url=message.attachments[0].url)
-                    if message.stickers != []:
-                        embed.set_thumbnail(url=message.stickers[0].url)
-                    if message.reference:
-                        reference_msg = await message.channel.fetch_message(message.reference.message_id)
-                        if reference_msg.embeds and reference_msg.author == self.bot.user:
-                            reference_message_content = reference_msg.embeds[0].description
-                            reference_message_author = reference_msg.embeds[0].author.name
-                        elif reference_msg.author != self.bot.user:
-                            reference_message_content = reference_msg.content
-                            reference_message_author = reference_msg.author.name + \
+                            else:
+                                embed.set_footer(
+                                    text="{} / mID:{}".format(message.guild.name, message.id))
+                            if message.attachments != []:
+                                embed.set_image(url=message.attachments[0].url)
+                            if message.stickers != []:
+                                embed.set_thumbnail(url=message.stickers[0].url)
+                            if message.reference:
+                                reference_msg = await message.channel.fetch_message(message.reference.message_id)
+                                if reference_msg.embeds and reference_msg.author == self.bot.user:
+                                    reference_message_content = reference_msg.embeds[0].description
+                                    reference_message_author = reference_msg.embeds[0].author.name
+                                elif reference_msg.author != self.bot.user:
+                                    reference_message_content = reference_msg.content
+                                    reference_message_author = reference_msg.author.name + \
                                 '#'+reference_msg.author.discriminator
-                        reference_content = ""
-                        for string in reference_message_content.splitlines():
-                            reference_content += "> " + string + "\n"
-                        reference_value = "**@{}**\n{}".format(
-                            reference_message_author, reference_content)
-                        embed.add_field(
-                            name='返信', value=reference_value, inline=True)
+                                reference_content = ""
+                                for string in reference_message_content.splitlines():
+                                    reference_content += "> " + string + "\n"
+                                reference_value = "**@{}**\n{}".format(
+                                    reference_message_author, reference_content)
+                                embed.add_field(
+                                    name='返信', value=reference_value, inline=True)
 
-                    await message.channel.send(embed=embed)
+                            await channel.send(embed=embed)
             await message.add_reaction('✅')
 
     @app_commands.guild_only()
