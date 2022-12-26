d164c8022af81abc6775a9822f01fd373a1df855
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-12-26 12:48:37 +0900

    Flask8 Lint

--- cogs/help.py
+++ cogs/help.py
@@ -6,24 +6,23 @@ from discord import app_commands
 a = "Tips: /help コマンド名でコマンドを検索できます。"
 cmd = []
 
+
 class Help(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot: commands.Bot = bot
         for command in self.bot.tree.walk_commands():
             cmd.append(app_commands.Choice(name=command.name, value=command.name))
 
-    @discord.app_commands.choices(
-        cmd=cmd
-    )
+    @discord.app_commands.choices(cmd=cmd)
     @app_commands.command(name="help", description="helpを表示します。")
     async def help(self, i: discord.Interaction, cmd: str = None) -> None:
         if not cmd:
             he = discord.Embed(title="ページ2", color=0x3498DB)
             he.set_footer(text=a)
             ee = discord.Embed(
-                    title="ページ1",
-                    color=0x3498DB,
-                ).set_footer(text=a)
+                title="ページ1",
+                color=0x3498DB,
+            ).set_footer(text=a)
             num = 0
             for command in self.bot.tree.walk_commands():
                 num = num + 1
@@ -44,7 +43,7 @@ class Help(commands.Cog):
                         description=self.bot.tree.get_command(cmd).description,
                         color=0x3498DB,
                     )
-            )
+                )
         else:
             return await i.response.send_message(
                 embed=discord.Embed(
