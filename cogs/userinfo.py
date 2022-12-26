ebd2bf9691e792bb7da56f1c7ae542029c14be4e
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-12-26 13:17:10 +0900

    キャメルケースに

--- cogs/userinfo.py
+++ cogs/userinfo.py
@@ -4,16 +4,16 @@ from discord import app_commands
 import Paginator
 
 
-class Userinfo(commands.Cog):
+class user(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot: commands.Bot = bot
 
     @app_commands.describe(user="ユーザー")
-    @app_commands.command(name="userinfo", description="指定したユーザーの情報を返します。")
-    async def userinfo(self, i: discord.Interaction, user: discord.User):
+    @app_commands.command(name="user", description="指定したユーザーの情報を返します。")
+    async def user(self, i: discord.Interaction, user: discord.User):
         await i.response.send_message("利用できません。")
         # TODO
 
 
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(Userinfo(bot))
+    await bot.add_cog(user(bot))
--- cogs/voicetext.py
+++ cogs/voicetext.py
@@ -1,10 +1,10 @@
 from discord.ext import commands
 
 
-class voice_text(commands.Cog):
+class voiceText(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot: commands.Bot = bot
 
 
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(voice_text(bot))
+    await bot.add_cog(voiceText(bot))
