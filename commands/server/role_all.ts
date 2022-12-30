a1c2156461222b04a587e7042e58ab920a455ff0
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-12-30 14:28:51 +0900

    ✨new: role_all

---
+++ commands/server/role_all.ts
@@ -0,0 +1,101 @@
+import { Command, ChatInputCommand } from "@sapphire/framework";
+export class TextSpace extends Command {
+  public constructor(context: Command.Context, options: Command.Options) {
+    super(context, { ...options });
+  }
+
+  public override registerApplicationCommands(
+    registry: ChatInputCommand.Registry
+  ) {
+    registry.registerChatInputCommand((builder) =>
+      builder
+        .setName("role_all")
+        .setDescription("すべてのユーザーにロールをOOするサブコマンド。")
+        .addSubcommand((input) =>
+          input
+            .setName("add")
+            .setDescription("すべてのユーザーにロールを付与します。")
+            .addRoleOption((input) =>
+              input
+                .setName("ロール")
+                .setDescription("付与するロール")
+                .setRequired(true)
+            )
+            .addStringOption((input) =>
+              input
+                .setName("botにも付与するか")
+                .setDescription("botにも付与するか")
+                .addChoices(
+                  { name: "はい", value: "true" },
+                  { name: "いいえ", value: "false" }
+                )
+                .setRequired(true)
+            )
+        )
+        .addSubcommand((input) =>
+          input
+            .setName("remove")
+            .setDescription("すべてのユーザーにロールをはく奪します。")
+            .addRoleOption((input) =>
+              input
+                .setName("ロール")
+                .setDescription("はく奪するロール")
+                .setRequired(true)
+            )
+            .addStringOption((input) =>
+              input
+                .setName("botにもはく奪するか")
+                .setDescription("botにもはく奪するか")
+                .addChoices(
+                  { name: "はい", value: "true" },
+                  { name: "いいえ", value: "false" }
+                )
+                .setRequired(true)
+            )
+        )
+    );
+  }
+
+  public async chatInputRun(interaction: Command.ChatInputInteraction) {
+    if (!interaction.guild)
+      return await interaction.reply({
+        content: "ここはサーバーではありません。",
+        ephemeral: true,
+      });
+    if (interaction.options.getSubcommand() === "add") {
+      const role = interaction.options.getRole("ロール");
+      interaction.guild.members.fetch().then((members) =>
+        Promise.all(members.map((member) => member.roles.add(`${role?.id}`)))
+          .then(async () => {
+            return await interaction.reply({
+              content: "ロールの付与に成功しました。",
+              ephemeral: true,
+            });
+          })
+          .catch(async () => {
+            return await interaction.reply({
+              content: "ロールの付与に失敗しました。",
+              ephemeral: true,
+            });
+          })
+      );
+    } else if (interaction.options.getSubcommand() === "remove") {
+      const role = interaction.options.getRole("ロール");
+      interaction.guild.members.fetch().then((members) =>
+        Promise.all(members.map((member) => member.roles.remove(`${role?.id}`)))
+          .then(async () => {
+            return await interaction.reply({
+              content: "ロールの剥奪に成功しました。",
+              ephemeral: true,
+            });
+          })
+          .catch(async () => {
+            return await interaction.reply({
+              content: "ロールの剥奪に失敗しました。",
+              ephemeral: true,
+            });
+          })
+      );
+    }
+  }
+}
