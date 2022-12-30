743dd964a8d9d1c4987aa3626b77f81e6bf3ae55
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-12-30 16:28:47 +0900

    ✨new: http server

--- index.ts
+++ index.ts
@@ -1,11 +1,20 @@
 import { SapphireClient } from "@sapphire/framework";
 import { Intents } from "discord.js";
 import { config } from "dotenv";
+import http from "node:http";
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.end("<h1>Botは動いています。</h1>");
+});
+
 const client = new SapphireClient({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS],
 });
 // replitで動かす場合は6行目を消してください。
 config();
+server.listen(8080);
+
 if (process.env.test_mode === "y") {
   console.info("\x1b[46m\x1b[30mInfo\x1b[49m\x1b[39m Log in in Test mode.");
   client.login(process.env.test_token);
