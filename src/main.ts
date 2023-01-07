a66b1d99822f98d1060976f2b1f0d7061c12b8a9
gx1285 <78240988+gx1285@users.noreply.github.com>
2023-01-07 19:12:45 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ client.once("ready", async () => {
 
 client.on("interactionCreate", async (interaction: Interaction) => {
   if (interaction.isApplicationCommand()) {
-    if (interaction.name === "ping") {
+    if (interaction.subCommand === "ping") {
       await interaction.reply({
         embeds: [
           new Embed()
@@ -30,7 +30,7 @@ client.on("interactionCreate", async (interaction: Interaction) => {
         ephemeral: true,
       });
     }
-    if (interaction.name === "invite") {
+    if (interaction.subCommand === "invite") {
       const user_id: string = interaction.options[0].value;
       const user: User =
         (await interaction.client.users.get(user_id)) || interaction.user;
